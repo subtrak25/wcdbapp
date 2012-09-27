7f79af2eba0afd15ab89e275b59b16f83bf6a038
subtrak25 <subtrak25@me.com>
2012-09-27 10:52:43 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wcdbapp::Application.config.secret_token = 'ea639033c1dd600d44986c51a8a5be8470e9b77035925d60c022c4d3b79d7b5697335b12adde65fbf1e13326ff3194e42f8832acd2f74cf0760e1a47cf52c330'
